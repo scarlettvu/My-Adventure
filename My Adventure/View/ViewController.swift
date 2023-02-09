a08876c9609782ece0377ad93b36f04d87412b7e
Scarlett Vu <uscarlettv@gmail.com>
2023-02-08 17:39:16 -0800

    Fixed references

--- My Adventure/View/ViewController.swift
+++ My Adventure/View/ViewController.swift
@@ -12,10 +12,8 @@ struct Scenario {
 
     class ViewController: UIViewController {
         
-        @IBOutlet weak var background: UIImageView!
+        
         @IBOutlet weak var adventureQuestion: UILabel!
-        @IBOutlet weak var choiceOne: UIButton!
-        @IBOutlet weak var choiceTwo: UIButton!
         
         var adventureController = AdventureController()
         
@@ -25,6 +23,7 @@ struct Scenario {
             updateUI()
         }
         
+
         @IBAction func choiceSubmission(_ sender: UIButton) {
             adventureController.getNextStory(sender.currentTitle!)
             updateUI()
@@ -33,8 +32,8 @@ struct Scenario {
         
         func updateUI() {
             adventureQuestion.text = adventureController.getTitle()
-            choiceOne.setTitle(adventureController.getChoice1(), for: UIControl.State.normal)
-            choiceTwo.setTitle(adventureController.getChoice2(), for: UIControl.State.normal)
+         //   choiceOne.setTitle(adventureController.getChoice1(), for: UIControl.State.normal)
+         //   choiceTwo.setTitle(adventureController.getChoice2(), for: UIControl.State.normal)
             
         }
     }
